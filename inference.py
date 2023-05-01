116ecf75c1b3424d7fbcc2048d24089f240d40de
vikimark <mark.phurit@gmail.com>
2023-05-02 00:30:12 +0700

    added: updated model

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ from fastai.vision.all import *
 from pathlib import Path
 
 def predict(img):
-    learn_inf = load_learner('models/model2.pkl')
+    learn_inf = load_learner('models/model-2May.pkl')
     pred, pred_idx, probs = learn_inf.predict(img)
     return pred, float(probs[pred_idx])
 
